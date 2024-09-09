743e6f84964393d00649edef5f88dbfa897b14ef
gizemdemirtaass <gizzemdemirtas@gmail.com>
2024-09-09 22:12:15 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by gizem demirtas on 9.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
