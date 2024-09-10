12efaed07d4837e591e996881f5316f68494633f
gizemdemirtaass <116717342+gizemdemirtas@users.noreply.github.com>
2024-09-10 15:23:42 +0300

    footer view

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by gizem demirtas on 10.09.2024.
+//
+
+import Foundation
---
+++ Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,18 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by gizem demirtas on 10.09.2024.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FooterView()
+}
